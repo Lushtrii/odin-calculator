02740167a7028978765daac5f50c6f24613b3407
Sean Ford <seanford@protonmail.com>
2024-07-18 15:35:35 -0700

    Add calculation func stubs

---
+++ calculator.js
@@ -0,0 +1,16 @@
+"use strict"
+const add = (a, b) => {
+    return -1;
+}
+
+const subtract = (a, b) => {
+    return -1;
+}
+
+const multiply = (a, b) => {
+    return -1;
+}
+
+const divide = (a, b) => {
+    return -1;
+}
