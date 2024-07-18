b2b49a9ca35d40f77ea3cbdda865038f3d94039f
Sean Ford <seanford@protonmail.com>
2024-07-18 15:40:51 -0700

    Implement multiply

--- calculator.js
+++ calculator.js
@@ -3,9 +3,7 @@ const add = (a, b) => a + b;
 
 const subtract = (a, b) => a - b;
 
-const multiply = (a, b) => {
-    return -1;
-}
+const multiply = (a, b) => a * b;
 
 const divide = (a, b) => {
     return -1;
