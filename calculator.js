b310682a29cff62e06dcffa1f00bb87b8a5aa5d3
Sean Ford <seanford@protonmail.com>
2024-07-18 16:29:59 -0700

    Implement operate()

--- calculator.js
+++ calculator.js
@@ -7,3 +7,26 @@ const multiply = (a, b) => a * b;
 
 const divide = (a, b) => b === 0 ? undefined : a / b;
 
+const operate = (a, b, op) => {
+    const opCodes = {
+        "+": 0,
+        "-": 1,
+        "*": 2,
+        "/": 3,
+    };
+
+    switch (opCodes[op]) {
+        case 0:
+            return add(a, b);
+        case 1:
+            return subtract(a, b);
+        case 2:
+            return multiply(a, b);
+        case 3:
+            let result = divide(a, b);
+            if (!result) {
+                console.log("You can't divide by 0...");
+            }
+            return result;
+    }
+}
