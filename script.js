810fa8aef36964bf757fd263f5bda97dd257e252
Sean Ford <seanford@protonmail.com>
2025-10-29 19:24:36 -0700

    feat: create divide operation

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
   return num1 * num2;
 }
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    throw new Error("Cannot divide by 0");
+  }
+
+  return num1 / num2;
+}
